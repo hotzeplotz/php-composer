de2754569c6568c447a6eec2ab1d1b6bb465782d
andrea rota <a@xelera.eu>
2013-06-28 23:54:17 +0100

    use system-wide location for autoloader

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -15,7 +15,9 @@ function includeIfExists($file)
     return file_exists($file) ? include $file : false;
 }
 
-if ((!$loader = includeIfExists(__DIR__.'/../vendor/autoload.php')) && (!$loader = includeIfExists(__DIR__.'/../../../autoload.php'))) {
+// Debian specific: use autoloader code from sysem-wide composer-autoload directory
+// TODO: if loader fails, error message should be updated accordingly
+if ((!$loader = includeIfExists(__DIR__.'/../vendor/autoload.php')) && (!$loader = includeIfExists(__DIR__.'/../../../autoload.php')) && (!$loader = includeIfExists(__DIR__.'/composer-autoload/autoload.php'))) {
     echo 'You must set up the project dependencies, run the following commands:'.PHP_EOL.
         'curl -sS https://getcomposer.org/installer | php'.PHP_EOL.
         'php composer.phar install'.PHP_EOL;
